316ec9e3a0c4940761e110c18bcd919fc2f5178c
akshaya4r <akshaya4r@gmail.com>
2022-03-18 18:25:23 +0100

    Plot dendrogram

--- R/atacCNV.R
+++ R/atacCNV.R
@@ -1,4 +1,4 @@
-#' Wrapper function for the \code{\link{atacCNV}} package
+#' Wrapper function for the \code{\link{epiAneufinder}} package
 #'
 #' @param input Folder with bam files or a fragments.tsv file
 #' @param outdir Path to output directory
@@ -8,7 +8,6 @@
 #' @param test One of AD or KS. Anderson-Darling or Kolmogorov-Smirnov. Default: "AD"
 #' @param reuse.existing Logical. False removes all the files in the outdir and recomputes everything.
 #' @param exclude String of chromosomes to exclude. Example: c('chrX','chrY','chrM')
-#' @param readout String. One of "ATAC", "BS". Default: "ATAC"
 #' @param uq Upper quantile. Default: 0.1
 #' @param lq Lower quantile. Default: 0.9
 #' @param title_karyo String. Title of the output karyogram
@@ -40,27 +39,10 @@
 #' @author Akshaya Ramakrishnan
 #' @export
 
-
-# Imports:
-# GenomicAlignments (>= 1.22.1),
-# Matrix (>= 1.2.18),
-# SummarizedExperiment (>= 1.16.1),
-# data.table (>= 1.12.8),
-# plyranges (>= 1.6.10),
-# Rsamtools (>= 2.2.1),
-# GenomeInfoDb (>= 1.22.0),
-# BSgenome.Hsapiens.UCSC.hg38 (>= 1.4.1),
-# GenomicRanges (>= 1.38.0),
-# stats (>= 3.6.1),
-# Biostrings (>= 2.54.0),
-# magrittr (>= 1.5),
-# BiocGenerics (>= 0.32.0),
-# S4Vectors (>= 0.24.2)
-
-atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapiens.UCSC.hg38",
-                    test='AD', reuse.existing=FALSE, exclude=NULL, readout="ATAC",
-                    uq=0.9, lq=0.5, somyl=0.1, somyu=0.8, title_karyo=NULL, minFrags = 20000,
-                    gene.annotation=NULL, threshold_blacklist_bins=0.85, ncores=4, minsize=1, k=3){
+epiAneufinder <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapiens.UCSC.hg38",
+                    test='AD', reuse.existing=FALSE, exclude=NULL,
+                    uq=0.9, lq=0.1, title_karyo=NULL, minFrags = 20000,
+                    threshold_blacklist_bins=0.85, ncores=4, minsize=1, k=3){
 
   if(reuse.existing==FALSE){
     print("Removing old file from the output folder")
@@ -106,6 +88,7 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
   if(!file.exists(file.path(outdir,"counts_gc_corrected.rds"))) {
     message("Correcting for GC bias...")
     corrected_counts <- peaks[, mclapply(.SD, function(x) {
+      # LOESS correction for GC
       fit <- stats::loess(x ~ peaks$GC)
       correction <- mean(x) / fit$fitted
       as.integer(round(x * correction))
@@ -118,6 +101,7 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
 
   zeroes_per_bin <- peaks[, rowSums(.SD==0), .SDcols = patterns("cell-")]
   ncells <- length(grep("cell-", colnames(peaks)))
+  # Exclude bins that have no signal in most cells
   peaks <- peaks[zeroes_per_bin<(threshold_blacklist_bins*ncells)]
 
   if(!file.exists(file.path(outdir,"results_gc_corrected.rds"))) {
@@ -138,6 +122,7 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
     clusters_ad <- readRDS(file.path(outdir,"results_gc_corrected.rds"))
     breakpoints <- lapply(clusters_ad, function(x) { lapply(x,'[[', 1) })
     distances <- lapply(clusters_ad, function(x) { lapply(x,'[[', 2) })
+    # Combine the braekpoints and distances in one dataframe
     result.dt <- Map(function(bp, dist){
       names(bp) <- names_seq
       names(dist) <- names_seq
@@ -147,7 +132,8 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
       per_cell_dt <- rbindlist(dtlist, idcol = 'Chr')
       per_cell_dt <- per_cell_dt[order(-per_chr_dist)]
     }, breakpoints, distances)
-
+    
+    # Discard irrelevant breakpoints
     pruned_result.dt <- lapply(result.dt, function(x){
       threshold_dist_values(x)
     })
@@ -155,8 +141,11 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
 
 
     clusters_pruned <- as.data.table(Map(function(seq_data, bp){
+      # Split the readcounts into the different chr
       per_chrom_seq_data <- split(seq_data, peaks$seqnames)
+      # Split the calculated breakpoints into the different chr
       per_chrom_bp <- split(bp, bp$Chr)
+      # Identify the differnt segments per chromosome after the pruning
       clusters_per_chrom <- Map(function(seq_data2, bp2){
         if(is.null(bp2)){
           return(rep(1, length(seq_data2)))
@@ -165,7 +154,9 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
           return(rep(1:length(diff(bp_to_cluster)),diff(bp_to_cluster)))
         }
       }, per_chrom_seq_data[names_seq], per_chrom_bp[names_seq])
-      cl <- 0
+      
+      # Identify the segments at the genome level after the pruning
+      cl <- 0 # counter
       clusters <- vector()
       for(item in clusters_per_chrom){
         item <- item + cl
@@ -175,30 +166,31 @@ atacCNV <- function(input, outdir, blacklist, windowSize, genome="BSgenome.Hsapi
       return(clusters)
     },  peaks[, .SD, .SDcols = patterns("cell-")], pruned_result.dt))
 
-    if(readout=="ATAC"){
-      somies_ad <- Map(function(seq_data,cluster) {
-        assign_gainloss(seq_data, cluster, uq=uq, lq=lq)
-      }, peaks[, .SD, .SDcols = patterns("cell-")], clusters_pruned)
-      print("Successfully assigned gain-loss")
-    }
-    if(readout=="BS"){
-      somies_ad <- Map(function(seq_data,cluster) {
-        assign_somy(seq_data, cluster, uq=uq, lq=lq, somyl=somyl, somyu=somyu)
-      }, peaks[, .SD, .SDcols = patterns("cell-")], clusters_pruned)
-      print("Successfully assigned gain-loss")
-    }
+    # Assign copy number states to the different "clusters"/segments identified
+    somies_ad <- Map(function(seq_data,cluster) {
+      assign_gainloss(seq_data, cluster, uq=uq, lq=lq)
+    }, peaks[, .SD, .SDcols = patterns("cell-")], clusters_pruned)
+    print("Successfully assigned gain-loss")
     saveRDS(somies_ad, file.path(outdir, "cnv_calls.rds"))
   }
-
+  
   somies_ad <- readRDS(file.path(outdir,"cnv_calls.rds"))
+  # Write the results to disk
+  write_somies.dt <- as.data.table(somies_ad)
+  write_somies.dt <- as.data.table(lapply(write_somies.dt, function(x) {
+    # x <- factor(x, levels = c(0,1,2), labels=c("Loss", "Normal", "Gain"))
+    x <- factor(x, levels = c(0,1,2))
+    return(x)
+    }))
+  write_somies.dt <- as.data.table(cbind(seq=peaks$seqnames, start=peaks$start, end=peaks$end, write_somies.dt))
+  write.table(write_somies.dt, file = file.path(outdir, "results_table.tsv"), quote = FALSE)
+  message("A .tsv file with the results has been written to disk. It contains the copy number states for each cell per bin.
+          0 denotes 'Loss', 1 denotes 'Normal', 2 denotes 'Gain'.")
+  
   if(is.null(title_karyo)){
     title_karyo <- basename(outdir)
   }
-  if(readout=="ATAC"){
-    plot_karyo_gainloss(somies_ad = somies_ad, outdir = outdir, peaks = peaks, uq, lq, title_karyo)
-  }
-  if(readout=="BS"){
-    plot_karyo(somies_ad = somies_ad, outdir = outdir, peaks = peaks, uq, lq, somyl, somyu, title_karyo)
-  }
+  # Call plotting function to plot and save karyogram
+  plot_karyo_gainloss(somies_ad = somies_ad, outdir = outdir, peaks = peaks, uq, lq, title_karyo)
   print("Successfully plotted karyogram")
 }
